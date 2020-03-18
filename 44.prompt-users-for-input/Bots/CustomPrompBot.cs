68dc98540dd5c59fddc7d4319d613cd8655897c8
Daniel Rosas Mialma <dan11_daniel@hotmail.com>
2020-03-18 03:06:37 -0600

    Dos

---
+++ 44.prompt-users-for-input/Bots/CustomPrompBot.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs.Choices;
+using Microsoft.Bot.Schema;
+using Microsoft.BotBuilderSamples;
+using Microsoft.Recognizers.Text;
+using Microsoft.Recognizers.Text.DateTime;
+using Microsoft.Recognizers.Text.Number;
+
+
+namespace PromptUsersForInput.Bots
+{
+    public class CustomPrompBot : ActivityHandler
+    {
+        private readonly BotState _userState;
+        private readonly BotState _conversationState;
+        static int AuxCoronavirus = 0;
+      
+      
+
+        public CustomPrompBot(ConversationState conversationState, UserState userState)
+        {
+            _conversationState = conversationState;
+            _userState = userState;
+        }
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+
+            var conversationStateAccessors = _conversationState.CreateProperty<ConversationFlow>(nameof(ConversationFlow));
+            var flow = await conversationStateAccessors.GetAsync(turnContext, () => new ConversationFlow());
+
+            var userStateAccessors = _userState.CreateProperty<UserProfile>(nameof(UserProfile));
+            var profile = await userStateAccessors.GetAsync(turnContext, () => new UserProfile());
+
+            await FillOutUserProfileAsync(flow, profile, turnContext);
+
+            // Save changes.
+            await _conversationState.SaveChangesAsync(turnContext);
+            await _userState.SaveChangesAsync(turnContext);
+        }
+
+        private static async Task FillOutUserProfileAsync(ConversationFlow flow, UserProfile profile, ITurnContext turnContext)
+        {
+           
+            string input = turnContext.Activity.Text?.Trim();
+            string message;
+            List<string> QuestionCoronavirus = new List<string>()
+            { "Tienes fiebre mayor a 38 grados?", "Se Tiene dolor de Cabeza?", "Se tiene tos?", "Se tiene dolor de cuerpo", "Lugares con muchas personas?" };
+            
+            switch (flow.LastQuestionAsked)
+            {
+                case ConversationFlow.Question.None:
+                        await turnContext.SendActivityAsync("Bienvenido");
+                        await Task.Delay(1000);
+                        await turnContext.SendActivityAsync(ChoiceFactory.HeroCard(new List<Choice>()
+                        { new Choice() { Value = "Yes" }, new Choice() { Value = "No" } }, QuestionCoronavirus[AuxCoronavirus]));
+                        AuxCoronavirus++;
+                        flow.LastQuestionAsked = ConversationFlow.Question.Coronavirus;
+                    break;
+                case ConversationFlow.Question.Coronavirus:
+                    if (input.Contains("Y"))
+                    {
+                        if (AuxCoronavirus == QuestionCoronavirus.Count)
+                        {
+                            await turnContext.SendActivityAsync("Posible Caso de Coronavirus");
+                            AuxCoronavirus = 0;
+                        }
+                        await turnContext.SendActivityAsync(ChoiceFactory.HeroCard(new List<Choice>()
+                        { new Choice() { Value = "Yes" }, new Choice() { Value = "No" } }, QuestionCoronavirus[AuxCoronavirus]));
+                        AuxCoronavirus++;
+                        flow.LastQuestionAsked = ConversationFlow.Question.Coronavirus;
+                    }
+                    else
+                    {
+                        await turnContext.SendActivityAsync("Consulta un Medico");
+                        flow.LastQuestionAsked = ConversationFlow.Question.Name;
+                    }
+                    break;
+                case ConversationFlow.Question.ResfriadoComun:
+                    break;
+                case ConversationFlow.Question.Name:
+                    if (ValidateName(input, out string name, out message))
+                    {
+                        profile.Name = name;
+                        await turnContext.SendActivityAsync($"Hi {profile.Name}.");
+                        await turnContext.SendActivityAsync("How old are you?");
+                        flow.LastQuestionAsked = ConversationFlow.Question.Age;
+                        break;
+                    }
+                    else
+                    {
+                        await turnContext.SendActivityAsync(message ?? "I'm sorry, I didn't understand that.");
+                        break;
+                    }
+                case ConversationFlow.Question.Age:
+                    if (ValidateAge(input, out int age, out message))
+                    {
+                        profile.Age = age;
+                        await turnContext.SendActivityAsync($"I have your age as {profile.Age}.");
+                        await turnContext.SendActivityAsync("When is your flight?");
+                        flow.LastQuestionAsked = ConversationFlow.Question.Date;
+                        break;
+                    }
+                    else
+                    {
+                        await turnContext.SendActivityAsync(message ?? "I'm sorry, I didn't understand that.");
+                        break;
+                    }
+
+                case ConversationFlow.Question.Date:
+                    if (ValidateDate(input, out string date, out message))
+                    {
+                        profile.Date = date;
+                        await turnContext.SendActivityAsync($"Your cab ride to the airport is scheduled for {profile.Date}.");
+                        await turnContext.SendActivityAsync($"Thanks for completing the booking {profile.Name}.");
+                        await turnContext.SendActivityAsync($"Type anything to run the bot again.");
+                        flow.LastQuestionAsked = ConversationFlow.Question.None;
+                        profile = new UserProfile();
+                        break;
+                    }
+                    else
+                    {
+                        await turnContext.SendActivityAsync(message ?? "I'm sorry, I didn't understand that.");
+                        break;
+                    }
+            }
+        }
+
+        private static bool ValidateName(string input, out string name, out string message)
+        {
+            name = null;
+            message = null;
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                message = "Please enter a name that contains at least one character.";
+            }
+            else
+            {
+                name = input.Trim();
+            }
+
+            return message is null;
+        }
+
+        private static bool ValidateAge(string input, out int age, out string message)
+        {
+            age = 0;
+            message = null;
+
+            // Try to recognize the input as a number. This works for responses such as "twelve" as well as "12".
+            try
+            {
+                // Attempt to convert the Recognizer result to an integer. This works for "a dozen", "twelve", "12", and so on.
+                // The recognizer returns a list of potential recognition results, if any.
+
+                var results = NumberRecognizer.RecognizeNumber(input, Culture.English);
+
+                foreach (var result in results)
+                {
+                    // The result resolution is a dictionary, where the "value" entry contains the processed string.
+                    if (result.Resolution.TryGetValue("value", out object value))
+                    {
+                        age = Convert.ToInt32(value);
+                        if (age >= 18 && age <= 120)
+                        {
+                            return true;
+                        }
+                    }
+                }
+
+                message = "Please enter an age between 18 and 120.";
+            }
+            catch
+            {
+                message = "I'm sorry, I could not interpret that as an age. Please enter an age between 18 and 120.";
+            }
+
+            return message is null;
+        }
+
+        private static bool ValidateDate(string input, out string date, out string message)
+        {
+            date = null;
+            message = null;
+
+            // Try to recognize the input as a date-time. This works for responses such as "11/14/2018", "9pm", "tomorrow", "Sunday at 5pm", and so on.
+            // The recognizer returns a list of potential recognition results, if any.
+            try
+            {
+                var results = DateTimeRecognizer.RecognizeDateTime(input, Culture.English);
+
+                // Check whether any of the recognized date-times are appropriate,
+                // and if so, return the first appropriate date-time. We're checking for a value at least an hour in the future.
+                var earliest = DateTime.Now.AddHours(1.0);
+
+                foreach (var result in results)
+                {
+                    // The result resolution is a dictionary, where the "values" entry contains the processed input.
+                    var resolutions = result.Resolution["values"] as List<Dictionary<string, string>>;
+
+                    foreach (var resolution in resolutions)
+                    {
+                        // The processed input contains a "value" entry if it is a date-time value, or "start" and
+                        // "end" entries if it is a date-time range.
+                        if (resolution.TryGetValue("value", out string dateString)
+                            || resolution.TryGetValue("start", out dateString))
+                        {
+                            if (DateTime.TryParse(dateString, out DateTime candidate)
+                                && earliest < candidate)
+                            {
+                                date = candidate.ToShortDateString();
+                                return true;
+                            }
+                        }
+                    }
+                }
+
+                message = "I'm sorry, please enter a date at least an hour out.";
+            }
+            catch
+            {
+                message = "I'm sorry, I could not interpret that as an appropriate date. Please enter a date at least an hour out.";
+            }
+
+            return false;
+        }
+    }
+}
---
+++ 44.prompt-users-for-input/ConversationFlow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.BotBuilderSamples
+{
+    public class ConversationFlow
+{
+    // Identifies the last question asked.
+    public enum Question
+    {
+        Name,
+        Age,
+        Date,
+        None, // Our last action did not involve a question.
+        Coronavirus,
+        ResfriadoComun,
+        Tos,
+        Infeccion,
+        Final
+    }
+
+    // The last question asked.
+    public Question LastQuestionAsked { get; set; } = Question.None;
+}
+}
